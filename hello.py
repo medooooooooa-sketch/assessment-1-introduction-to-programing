40bcffd90a27850cf8b72c4116e74cb1d045288a
medooooooooa-sketch <medoooooooo.a@gmail.com>
2026-01-06 17:15:44 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,11 @@
+# Function that prints a greeting
+def hello():
+    print("Hello")
+
+# Main function (program entry point)
+def main() -> None:
+    hello()
+
+# Run main only if this file is executed directly
+if __name__ == "__main__":
+    main()
