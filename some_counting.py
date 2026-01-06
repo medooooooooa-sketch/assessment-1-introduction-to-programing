bb151ee21a9bd68508c59fab6ec7f8e0501ffd03
medooooooooa-sketch <medoooooooo.a@gmail.com>
2026-01-06 17:16:35 +0300

    Add files via upload

---
+++ some_counting.py
@@ -0,0 +1,20 @@
+
+# Print numbers from 0 to 50
+for i in range(0, 51):
+    print(i)
+
+# Print numbers from 50 down to 0
+for i in range(50, -1, -1):
+    print(i)
+
+# Print numbers from 30 to 50
+for i in range(30, 51):
+    print(i)
+
+# Print even numbers from 50 down to 10
+for i in range(50, 9, -2):
+    print(i)
+
+# Print numbers from 100 to 200 with step 5
+for i in range(100, 201, 5):
+    print(i)
