a93ac9c391660c90f22b5e3020e6a8552f9e0ad7
medooooooooa-sketch <medoooooooo.a@gmail.com>
2026-01-06 17:14:16 +0300

    Add files via upload

---
+++ Biography.py
@@ -0,0 +1,10 @@
+# Get user information
+full_name = input("enter your name")
+city = input("enter your city")
+age = input("enter your age")
+
+# Check if age contains only numbers
+if age.isdigit():
+    age = int(age)  # Convert age to integer
+else:
+    print("error")  # Invalid age input
