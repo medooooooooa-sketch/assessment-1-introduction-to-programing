b5c33cc461130a2a978a16bade2ccb1789ebc147
medooooooooa-sketch <medoooooooo.a@gmail.com>
2026-01-06 17:15:59 +0300

    Add files via upload

---
+++ primitive_quiz.py
@@ -0,0 +1,98 @@
+# Ask about the capital of France
+city = input("What is the capital of France? ")
+capital = "paris"
+
+if city.lower() == capital:
+    print("Correct answer!")
+else:
+    print("Your answer is incorrect!")
+
+
+# Ask about the capital of Germany
+city = input("What is the capital of Germany? ")
+capital = "berlin"
+
+if city.lower() == capital:
+    print("Correct answer!")
+else:
+    print("Your answer is incorrect!")
+
+
+# Ask about the capital of Italy
+city = input("What is the capital of Italy? ")
+capital = "rome"
+
+if city.lower() == capital:
+    print("Correct answer!")
+else:
+    print("Your answer is incorrect!")
+
+
+# Ask about the capital of Spain
+city = input("What is the capital of Spain? ")
+capital = "madrid"
+
+if city.lower() == capital:
+    print("Correct answer!")
+else:
+    print("Your answer is incorrect!")
+
+
+# Ask about the capital of Portugal
+city = input("What is the capital of Portugal? ")
+capital = "lisbon"
+
+if city.lower() == capital:
+    print("Correct answer!")
+else:
+    print("Your answer is incorrect!")
+
+
+# Ask about the capital of the Netherlands
+city = input("What is the capital of the Netherlands? ")
+capital = "amsterdam"
+
+if city.lower() == capital:
+    print("Correct answer!")
+else:
+    print("Your answer is incorrect!")
+
+
+# Ask about the capital of Belgium
+city = input("What is the capital of Belgium? ")
+capital = "brussels"
+
+if city.lower() == capital:
+    print("Correct answer!")
+else:
+    print("Your answer is incorrect!")
+
+
+# Ask about the capital of Austria
+city = input("What is the capital of Austria? ")
+capital = "vienna"
+
+if city.lower() == capital:
+    print("Correct answer!")
+else:
+    print("Your answer is incorrect!")
+
+
+# Ask about the capital of Ireland
+city = input("What is the capital of Ireland? ")
+capital = "dublin"
+
+if city.lower() == capital:
+    print("Correct answer!")
+else:
+    print("Your answer is incorrect!")
+
+
+# Ask about the capital of Poland
+city = input("What is the capital of Poland? ")
+capital = "warsaw"
+
+if city.lower() == capital:
+    print("Correct answer!")
+else:
+    print("Your answer is incorrect!")
